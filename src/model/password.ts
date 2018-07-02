d19115ad147ce5eba8c72293b210cc6d0b7d6386
Jae Hyeon Park <usezmap@gmail.com>
2018-07-02 19:16:35 +0900

    password.ts implementation (#1)

--- src/model/password.ts
+++ src/model/password.ts
@@ -1,20 +1,31 @@
+import * as bcrypt from 'bcrypt';
+import * as crypto from 'crypto';
 /**
  * Check for password
  */
-export async function check(stored: Buffer, input: string): Promise<boolean> {
-  throw new Error('Not implemented');
+export async function check(stored: string, input: string): Promise<boolean> {
+  return bcrypt.compare(stored, input);
 }
 
 /**
  * Encrypt
  */
-export async function encrypt(input: string, iterations: number): Promise<Buffer> {
-  throw new Error('Not implemented');
+export async function encrypt(input: string, iterations: number): Promise<string> {
+  return bcrypt.hash(input, 10);
 }
 
 /**
  * Generate secure randomString for resetToken, etc.
  */
 export async function random(): Promise<string> {
-  throw new Error('Not implemented');
+  return new Promise<string>((resolve, reject) => {
+    // this will generate 40 length of string
+    crypto.randomBytes(20, (err, buffer) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(buffer.toString('hex'));
+    })
+  });
 }
