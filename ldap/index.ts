76438a93099e49d65d4c3c3afc18147d85e4b194
Jangho Seo <jangho@jangho.io>
2018-07-21 17:30:45 +0900

    LDAP works

--- ldap/index.ts
+++ ldap/index.ts
@@ -1,3 +1,54 @@
 import * as ldap from 'ldapjs'
 
 const server = ldap.createServer()
+const users: Array<ldap.Entity> = [
+  {
+    dn: 'cn=bacchus,ou=cseusers,dc=snucse,dc=org',
+    attributes: {
+      uid: 'bacchus',
+      cn: 'bacchus',
+      gecos: 'AdminDescription',
+      homeDirectory: '/home/bacchus',
+      loginShell: '/bin/bash',
+      objectclass: 'posixAccount',
+      uidNumber: 10000,
+      gidNumber: 1004,
+    }
+  }, {
+    dn: 'cn=master,ou=cseusers,dc=snucse,dc=org',
+    attributes: {
+      cn: 'master',
+      uid: 10001,
+      gid: 10001,
+      description: 'Admin2',
+      homedirectory: '/home/master',
+      shell: '/bin/bash',
+      objectclass: 'unixUser',
+    }
+  }
+]
+
+server.listen(389, '127.0.0.1', () => console.log('Connected'))
+server.bind('ou=cseusers,dc=snucse,dc=org', (req, res, next) => {
+  console.log(req.dn.toString())
+  if (req.dn.toString() !== 'cn=bacchus, ou=cseusers, dc=snucse, dc=org' || req.credentials !== 'password') {
+    return next(new ldap.InvalidCredentialsError())
+  }
+  res.end()
+  return next()
+})
+server.search('ou=cseusers, dc=snucse, dc=org', (req, res, next) => {
+  if (req.dn.toString() === 'ou=cseusers, dc=snucse, dc=org') {
+    res.send(users[0])
+  }
+  res.end()
+  return next()
+})
+
+server.search('cn=bacchus,ou=cseusers, dc=snucse, dc=org', (req, res, next) => {
+  if (req.dn.toString() === 'cn=bacchus, ou=cseusers, dc=snucse, dc=org') {
+    res.send(users[0])
+  }
+  res.end()
+  return next()
+})
--- typings/ldapjs/index.d.ts
+++ typings/ldapjs/index.d.ts
@@ -1,29 +1,79 @@
 /// <reference types="bunyan" />
 
 declare module 'ldapjs' {
+
   import * as Bunyan from 'bunyan'
+
+  export interface Error {
+  }
 
   export interface ServerOptions {
     /**
      * A bunyan instance to be used for logging.
      */
-    log: Bunyan,
+    log?: Bunyan,
     /**
      * PEM-encoded X.509 certificate.
      */
-    certificate: string,
+    certificate?: string,
     /**
      * PEM-encoded private key for the certificate.
      */
-    key: string,
+    key?: string,
+  }
+
+  export interface DN {
+    toString(): string
+  }
+
+  export interface ServerHandler {
+    (req: Req, res: Res, next: (error?: Error) => ServerHandler): void
+  }
+
+  export interface Req {
+    dn: DN,
+    filter: Filter,
+    credentials: string,
+  }
+
+  export interface Res {
+    end(): void,
+    send(entity: Entity): void
+  }
+
+  export interface Entity {
+    dn: string,
+    attributes: object,
+  }
+
+  export interface Filter {
+    matches(entity: object): boolean
   }
 
   export interface Server {
     /**
-     * This function blahblah.
-     * @param x is awesome
+     * Begin accepting connections on the given port and host.
+     * @param port port to bind.
+     * @param host host to bind. (default: any IPv4 address)
+     * @param callback callback to be called when the server has been bound
      */
-    test(x: number): number
+    listen(port: number, host?: string, callback?: () => void): void
+
+    /**
+     * Chain handler to Bind operation
+     * @param name the DN to mount this handler
+     * @param handlers handlers
+     */
+    bind(name: string, ...handlers: Array<ServerHandler>): Server
+
+    /**
+     * Chain handler to Search operation
+     * @param name the DN to mount this handler
+     * @param handlers handlers
+     */
+    search(name: string, ...handlers: Array<ServerHandler>): Server
+
+    use(...handlers: Array<ServerHandler>): Server
   }
 
   /**
@@ -31,4 +81,10 @@ declare module 'ldapjs' {
    * @param options options to provide.
    */
   export function createServer(options?: ServerOptions): Server
+
+  export class InvalidCredentialsError implements Error {
+  }
+
+  export class InsufficientAccessRightsError implements Error {
+  }
 }
