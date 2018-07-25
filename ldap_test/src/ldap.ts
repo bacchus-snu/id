8018f365c38e6481d098991ad0af3d230b0ed371
Jangho Seo <jangho@jangho.io>
2018-07-26 00:54:06 +0900

    Filters, Requets, responses

--- ldap_test/src/ldap.ts
+++ ldap_test/src/ldap.ts
@@ -5,7 +5,7 @@ import { RootDSE } from './types'
 import { Subschema, subschemaObjectClass } from './types'
 
 const server = ldap.createServer()
-const subschema: ldap.Entity<Subschema> = {
+const subschema: ldap.SearchEntry<Subschema> = {
   dn: 'cn=subschema,dc=snucse,dc=org',
   attributes: {
     objectClass: subschemaObjectClass,
@@ -13,7 +13,7 @@ const subschema: ldap.Entity<Subschema> = {
     subtreeSpecification: '{ base "ou=cseusers,dc=snucse,dc=org" }',
   },
 }
-const users: Array<ldap.Entity<PosixAccount>> = [
+const users: Array<ldap.SearchEntry<PosixAccount>> = [
   {
     dn: 'cn=bacchus,ou=cseusers,dc=snucse,dc=org',
     attributes: {
@@ -41,7 +41,7 @@ const users: Array<ldap.Entity<PosixAccount>> = [
   },
 ]
 
-const cseusers: ldap.Entity<OrganizationalUnit> = {
+const cseusers: ldap.SearchEntry<OrganizationalUnit> = {
   dn: 'ou=cseusers,dc=snucse,dc=org',
   attributes: {
     objectClass: organizationalUnitObjectClass,
@@ -50,7 +50,7 @@ const cseusers: ldap.Entity<OrganizationalUnit> = {
   },
 }
 
-const rootDSE: ldap.Entity<RootDSE> = {
+const rootDSE: ldap.SearchEntry<RootDSE> = {
   dn: '',
   attributes: {
     namingContexts: 'ou=cseusers,dc=snucse,dc=org',
@@ -61,13 +61,16 @@ const rootDSE: ldap.Entity<RootDSE> = {
 
 server.listen(389, '127.0.0.1', () => console.log('Connected'))
 server.bind('ou=cseusers,dc=snucse,dc=org', (req, res, next) => {
-  const x = req.dn.rdns[0].attrs['cn'].value
-  console.log(x)
-  if (req.dn.toString() !== 'cn=bacchus, ou=cseusers, dc=snucse, dc=org' || req.credentials !== 'password') {
-    return next(new ldap.InvalidCredentialsError())
+  console.log(req)
+  if (req.dn.toString() === 'cn=bacchus, ou=cseusers, dc=snucse, dc=org' && req.credentials === 'bpassword') {
+    res.end()
+    return next()
   }
-  res.end()
-  return next()
+  if (req.dn.toString() === 'cn=master, ou=cseusers, dc=snucse, dc=org' && req.credentials === 'mpassword') {
+    res.end()
+    return next()
+  }
+  return next(new ldap.InvalidCredentialsError())
 })
 server.search('', (req, res, next) => {
   if (req.dn.toString() === '' && req.scope === 'base') {
@@ -84,12 +87,22 @@ server.search('cn=subschema, dc=snucse, dc=org', (req, res, next) => {
   return next()
 })
 server.search('ou=cseusers, dc=snucse, dc=org', (req, res, next) => {
+  //
+  // TODO: check req against the type definition
+  // console.log(req)
+  //
   if (req.dn.toString() === 'ou=cseusers, dc=snucse, dc=org') {
     if (req.scope === 'base') {
       res.send(cseusers)
     } else {
-      res.send(users[1])
-      res.send(users[0])
+      // Same results for 'one' and 'sub'
+      console.log(req.filter)
+      if (req.filter.matches(users[0])) {
+        res.send(users[0])
+      }
+      if (req.filter.matches(users[1])) {
+        res.send(users[1])
+      }
     }
   }
   res.end()
--- ldap_test/typings/ldapjs/index.d.ts
+++ ldap_test/typings/ldapjs/index.d.ts
@@ -3,6 +3,7 @@
 
 declare module 'ldapjs' {
 
+  import * as net from 'net'
   import * as Bunyan from 'bunyan'
 
   /**
@@ -28,7 +29,7 @@ declare module 'ldapjs' {
   /**
    * Distinguished name (RFC2253).
    */
-  export class DN {
+  export interface DN {
     /**
      * Array of RDNs which compose this DN, in the order of the presence in the string representation.
      */
@@ -72,7 +73,7 @@ declare module 'ldapjs' {
   /**
    * Relative distinguished name (RFC2253).
    */
-  export class RDN {
+  export interface RDN {
     /**
      * Map between RDN name in lowercase and the corresponding RDNAttribute.
      */
@@ -106,34 +107,188 @@ declare module 'ldapjs' {
   }
 
   /**
+   * Provides access to the next handler in the chain, and allows specifying an error during the operation.
+   * @param error an error during the operation
+   */
+  export type Next = (error?: Error) => void
+
+  /**
    * Handles requests from clients.
    * @param req provides information about the request
    * @param res provides contexts and methods for making responses
-   * @param next provides access for the next handler in the chain
+   * @param next provides access to the next handler in the chain
    */
-  export type Handler = (req: Req, res: Res, next: (error?: Error) => void) => void
+  export type Handler<ReqType, ResType> = (req: ReqType, res: ResType, next: Next) => void
 
+  /**
+   * Handles bind requests.
+   */
+  export type BindHandler = Handler<BindRequest, Res>
+
+  /**
+   * Handles search requests.
+   */
+  export type SearchHandler = Handler<SearchRequest, SearchResponse>
+
+  /**
+   * LDAP context for a connection.
+   */
+  export interface LDAPConnectionContext {
+    /**
+     * Specifies what the client have authenticated as.
+     */
+    bindDN: DN
+  }
+
+  /**
+   * Request to the server.
+   */
   export interface Req {
-    dn: DN,
-    filter: Filter,
-    credentials: string,
-    scope: 'base' | 'one' | 'sub'
+    /**
+     * The DN on which the request wants to operate.
+     */
+    dn: DN
+
+    /**
+     * Unique identifier for the connection-request pair.
+     */
+    logId: string
+
+    /**
+     * Provides information for the connection.
+     */
+    connection: net.Socket & {
+      ldap: LDAPConnectionContext
+    }
   }
 
+  /**
+   * Response from the server.
+   */
   export interface Res {
-    end(): void,
-    send(entity: Entity<object>): void
+    /**
+     * Send the result of LDAP operation to the client.
+     * To indicate any errors, consider passing an Error object to the 'next' function in the handler,
+     * instead of indicating erros by setting statusCode manually.
+     * @param statusCode status code (default: SUCCESS)
+     */
+    end(statusCode?: number): void
   }
 
-  export interface Entity<T> {
-    dn: DN | string,
-    attributes: T,
+  /**
+   * Bind request.
+   *
+   * Note back that 'name' property is omitted. The 'dn' property has the same value.
+   */
+  export interface BindRequest extends Req {
+    /**
+     * LDAP protocol version the client wants.
+     *
+     * ldapjs supports LDAP v3 only.
+     */
+    version: number
+
+    /**
+     * The authentication method.
+     *
+     * ldapjs supports 'simple' only.
+     */
+    authentication: string
+
+    /**
+     * For 'simple' authentication, it's the plain text for the password.
+     */
+    credentials: string
   }
 
+  /**
+   * Search request.
+   *
+   * Note back that 'baseObject' property is omitted. The 'dn' property has the same value.
+   */
+  export interface SearchRequest extends Req {
+    /**
+     * Scope of the search.
+     * The base object itself, one-level, or subtree.
+     */
+    scope: 'base' | 'one' | 'sub'
+
+    /**
+     * Specifies whether to dereference aliases or not (RFC 4511).
+     * neverDerefAliases(0), derefInSearching(1), derefFindingBaseObj(2), or derefAlways(3).
+     */
+    derefAliases: number
+
+    /**
+     * Limits the number of entires returned.
+     * 0 means unlimited.
+     */
+    sizeLimit: number
+
+    /**
+     * Limits the time to take in sending responses.
+     * 0 means unlimited.
+     */
+    timeLimit: number
+
+    /**
+     * Whether to return only names of the attributes or not.
+     * Automatically handled by ldapjs.
+     */
+    typesOnly: boolean
+
+    /**
+     * Filter object the client requested.
+     */
+    filter: Filter
+
+    /**
+     * Limits the attributes in the responses.
+     * Automatically handled by ldapjs.
+     */
+    attributes: Array<string>
+  }
+
+  /**
+   * Provides mechanism for filtering entries.
+   */
   export interface Filter {
-    matches(entity: object): boolean
+    /**
+     * @param entry the entry to test against
+     * @returns true if the entry matches with the filter, false otherwise
+     */
+    matches(entry: SearchEntry<object>): boolean
   }
 
+  /**
+   * Search response.
+   */
+  export interface SearchResponse extends Res {
+    /**
+     * Sends a response entry.
+     * @param entry entry to send
+     */
+    send(entry: SearchEntry<object>): void
+  }
+
+  /**
+   * Represents an entry in serach results.
+   */
+  export interface SearchEntry<T> {
+    /**
+     * DN of the object.
+     */
+    dn: DN | string,
+
+    /**
+     * Attributes of the object.
+     */
+    attributes: T
+  }
+
+  /**
+   * LDAP server.
+   */
   export interface Server {
     /**
      * Begin accepting connections on the given port and host.
@@ -144,18 +299,18 @@ declare module 'ldapjs' {
     listen(port: number, host?: string, callback?: () => void): void
 
     /**
-     * Chain handler to Bind operation
+     * Chain handler to bind operation
      * @param name the DN to mount this handler
      * @param handlers handlers
      */
-    bind(name: string, ...handlers: Array<Handler|Array<Handler>>): Server
+    bind(name: string, ...handlers: Array<BindHandler|Array<BindHandler>>): Server
 
     /**
-     * Chain handler to Search operation
+     * Chain handler to search operation
      * @param name the DN to mount this handler
      * @param handlers handlers
      */
-    search(name: string, ...handlers: Array<Handler|Array<Handler>>): Server
+    search(name: string, ...handlers: Array<SearchHandler|Array<SearchHandler>>): Server
   }
 
   /**
@@ -164,12 +319,47 @@ declare module 'ldapjs' {
    */
   export function createServer(options?: ServerOptions): Server
 
+  /**
+   * Represents an error occurred during the operation (RFC4511).
+   */
   export interface Error {
   }
 
+  /**
+   * Indicates that the received data is not well-formed.
+   * For bind operation only, this may also indicate that
+   * the server does not support the requested protocol version.
+   */
+  export class ProtocolError implements Error {
+  }
+
+  /**
+   * Indicates that the specified authentication method is not supported.
+   */
+  export class AuthMethodNotSupportedError implements Error {
+  }
+
+  /**
+   * Indicates that the object does not exist.
+   */
+  export class NoSuchObjectError implements Error {
+  }
+
+  /**
+   * Indicates that the provided credential is invalid.
+   */
   export class InvalidCredentialsError implements Error {
   }
 
+  /**
+   * Indicates that the client does not have sufficient access rights to request the operation.
+   */
   export class InsufficientAccessRightsError implements Error {
+  }
+
+  /**
+   * Indicates an internal error encountered by the server.
+   */
+  export class OtherError implements Error {
   }
 }
