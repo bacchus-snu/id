00ade82955fb9d5169460c8a10713027bbc8c8ad
Jangho Seo <jangho@jangho.io>
2018-08-04 20:03:33 +0900

    user

--- core/src/index.ts
+++ core/src/index.ts
@@ -20,6 +20,8 @@ ldapServer.listen(config.ldap.listenPort, config.ldap.listenHost,
   () => log.info(`LDAP server listening on ${config.ldap.listenHost}:${config.ldap.listenPort}`))
 const apiServer = createAPIServer(log, model)
 apiServer.listen(config.api.listenPort, config.api.listenHost)
-model.pgDo(c => model.emailAddresses.create(c, 'nobody', 'bacchus.snucse.org'))
-  .then(x => log.info(`Query OK: result ${x}`))
-  .catch(e => log.error(e, 'Query fail'))
+model.pgDo(async c => {
+  const emailAddressIdx = await model.emailAddresses.create(c, 'new', 'bacchus.snucse.org')
+  return await model.users.create(c, 'bacchus', 'password_digest', 'bacchus', emailAddressIdx)
+}).then(x => log.info(`Query OK: result ${x}`))
+.catch(e => log.error(e, 'Query fail'))
--- core/src/model/users.ts
+++ core/src/model/users.ts
@@ -1,9 +1,11 @@
 import { PoolClient } from 'pg'
 
 export default class Users {
-  public async create(client: PoolClient, username: string, name: string, password: string): Promise<void> {
-    const query = 'INSERT INTO users(username, name, password) VALUES ($1, $2, $3)'
-    const values = [username, name, password]
-    await client.query(query, values)
+  public async create(client: PoolClient, username: string, passwordDigest: string,
+      name: string, primaryEmailAddressIdx: number): Promise<number> {
+    const query = 'INSERT INTO users(username, password_digest, name, primary_email_address_idx)' +
+      'VALUES ($1, $2, $3, $4) RETURNING user_idx'
+    const result = await client.query(query, [username, passwordDigest, name, primaryEmailAddressIdx])
+    return result.rows[0].user_idx
   }
 }
