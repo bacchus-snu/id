e0b9a64eb0964a84a35e5870e2991d00c481f1a9
skystar-p <usezmap@gmail.com>
2018-08-04 19:32:57 +0900

    some settings

--- core/src/model/users.ts
+++ core/src/model/users.ts
@@ -5,9 +5,10 @@ export default class Users {
   constructor(private readonly model: Model) {
   }
 
-  public async create(client: PoolClient, username: string, name: string, password: string): Promise<void> {
+  public async create(_client: PoolClient, username: string, name: string, password: string): Promise<void> {
     const query = 'INSERT INTO users(username, name, password) VALUES ($1, $2, $3)'
     const values = [username, name, password]
-    await client.query(query, values)
+    // await client.query(query, values)
+    this.model.log.info(`INSERTING ${username}, ${name}, ${password}`)
   }
 }
