47a0fae2a7f83a48264818d9586b4755bc1d7903
Jae Hyeon Park <1418874+skystar-p@users.noreply.github.com>
2018-08-04 20:22:39 +0900

    Simple implementation of creating user (#10)

--- core/src/model/users.ts
+++ core/src/model/users.ts
@@ -1,9 +1,14 @@
+import Model from './model'
 import { PoolClient } from 'pg'
 
 export default class Users {
-  public async create(client: PoolClient, username: string, name: string, password: string): Promise<void> {
+  constructor(private readonly model: Model) {
+  }
+
+  public async create(_client: PoolClient, username: string, name: string, password: string): Promise<void> {
     const query = 'INSERT INTO users(username, name, password) VALUES ($1, $2, $3)'
     const values = [username, name, password]
-    await client.query(query, values)
+    // await client.query(query, values)
+    this.model.log.info(`INSERTING ${username}, ${name}, ${password}`)
   }
 }
