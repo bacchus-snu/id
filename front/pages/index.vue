38bd8f0931b1e397ca893555b37f1b2fe2003df3
skystar-p <usezmap@gmail.com>
2018-08-04 14:31:03 +0900

    testing with TestComponent

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,37 +1,21 @@
 <template>
   <section>
-    <h1 class="header">Nuxt TypeScript Starter</h1>
-    <div class="cards">
-      <Card v-for="person in people" :key="person.id" :person="person"></Card>
+    <div>
+      <test-component></test-component>
     </div>
   </section>
 </template>
 
 <script lang="ts">
-import {
-  Component,
-  Vue
-} from "nuxt-property-decorator"
-import { State } from "vuex-class"
-import Card from "~/components/Card.vue"
+import { Component, Vue } from 'nuxt-property-decorator';
+import TestComponent from '~/components/TestComponent.vue';
 
 @Component({
   components: {
-    Card
+    'test-component': TestComponent,
   }
 })
 export default class extends Vue {
-  @State people
+
 }
 </script>
-<style scoped>
-.header {
-  font-family: "Segoe UI", Tahoma, Geneva, Verdana,
-    sans-serif;
-}
-
-.cards {
-  display: flex;
-  flex-wrap: wrap;
-}
-</style>
