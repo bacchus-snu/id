760b0b59ce541b70cf22b2272e84829aa10788c7
Jangho Seo <jangho@jangho.io>
2018-08-04 20:46:22 +0900

    remove test code

--- core/src/index.ts
+++ core/src/index.ts
@@ -20,8 +20,3 @@ ldapServer.listen(config.ldap.listenPort, config.ldap.listenHost,
   () => log.info(`LDAP server listening on ${config.ldap.listenHost}:${config.ldap.listenPort}`))
 const apiServer = createAPIServer(log, model)
 apiServer.listen(config.api.listenPort, config.api.listenHost)
-model.pgDo(async c => {
-  const emailAddressIdx = await model.emailAddresses.create(c, 'new', 'bacchus.snucse.org')
-  return await model.users.create(c, 'bacchus', 'password_digest', 'bacchus', emailAddressIdx)
-}).then(x => log.info(`Query OK: result ${x}`))
-.catch(e => log.error(e, 'Query fail'))
