e89da015a17e255934df162efa32f242230c182f
Jangho Seo <jangho@jangho.io>
2018-08-04 16:56:44 +0900

    non-labmda testing

--- core/src/model/email_addresses.ts
+++ core/src/model/email_addresses.ts
@@ -10,11 +10,10 @@ export default class EmailAddresses {
    * @param domain domain part of the address
    * @return promise of the index of the new record
    */
-  public async create(local: string, domain: string): Promise<() => number> {
+  public async create(local: string, domain: string): Promise<number> {
     const query = 'INSERT INTO email_addresses(address_local, address_domain) VALUES ($1, $2)' +
       'RETURNING email_address_idx'
     const result = await this.model.pgClient.query(query, [local, domain])
-    this.model.log.info(result)
-    return () => result.rows[0].email_address_idx
+    return result.rows[0].email_address_idx
   }
 }
