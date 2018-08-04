42debef67fb52d27c8471b1fd8ca27a83261e39a
Jangho Seo <jangho@jangho.io>
2018-08-05 00:27:44 +0900

    ...

--- core/src/model/users.ts
+++ core/src/model/users.ts
@@ -64,7 +64,10 @@ export default class Users {
     if (getNewUidResult.rows.length !== 1) {
       throw new Error('Failed to assign posix uid')
     }
-    const newUid = getNewUidResult.rows[0] ? minUid : getNewUidResult.rows[0].uid
+    this.model.log.info(getNewUidResult)
+    this.model.log.info(getNewUidResult.rows)
+    this.model.log.info(getNewUidResult.rows[0])
+    const newUid = getNewUidResult.rows[0] ? getNewUidResult.rows[0].uid : minUid
     const assignResult = await client.query('UPDATE users SET uid = $1 WHERE user_idx = $2 AND uid IS NULL',
       [newUid, userIdx])
   }
