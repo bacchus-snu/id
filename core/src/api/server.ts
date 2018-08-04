1ab582720931357d193eb82ef9fa18900ff9e6db
skystar-p <usezmap@gmail.com>
2018-08-04 20:02:54 +0900

    user create api implement

--- core/src/api/server.ts
+++ core/src/api/server.ts
@@ -1,12 +1,37 @@
 import * as Koa from 'koa'
 import * as Bunyan from 'bunyan'
 import Model from '../model/model'
+import * as Router from 'koa-router'
+import * as bodyParser from 'koa-bodyparser'
 
-const createServer = (_log: Bunyan, _model: Model) => {
+const createServer = (log: Bunyan, model: Model) => {
   const app = new Koa()
-  app.use(async ctx => {
-    ctx.body = 'Hello, world'
+  const router = new Router()
+
+  app.use(bodyParser())
+
+  router.post('/api/user', async (ctx, next) => {
+    let body
+    try {
+      body = JSON.parse(ctx.request.body)
+    } catch (e) {
+      ctx.status = 400
+      return
+    }
+
+    let { username, name, password } = body
+
+    model.pgDo(c => model.users.create(c, username, name, password ))
+    ctx.status = 201
+    await next()
   })
+
+  app.on('error', e => {
+    log.error('API error', e)
+  })
+
+  app.use(router.routes()).use(router.allowedMethods())
+
   return app
 }
 
