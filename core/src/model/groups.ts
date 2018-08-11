c9105af1e32cf1fa27b4e2ed6a106868186dcfeb
Jae Hyeon Park <1418874+skystar-p@users.noreply.github.com>
2018-08-11 19:51:25 +0900

    Lets test DB (#24)
    ava로 테스트 프레임워크를 마련하고 db를 간단하게 테스트하는 코드를 추가

--- core/src/model/groups.ts
+++ core/src/model/groups.ts
@@ -3,6 +3,12 @@ import { Translation } from './translation'
 import { PoolClient } from 'pg'
 import { NoSuchEntryError } from './errors'
 
+export interface Group {
+  idx: number
+  name: Translation
+  description: Translation
+}
+
 export default class Groups {
   constructor(private readonly model: Model) {
   }
@@ -12,6 +18,15 @@ export default class Groups {
       'description_en) VALUES ($1, $2, $3, $4) RETURNING idx'
     const result = await client.query(query, [name.ko, name.en, description.ko, description.en])
     return result.rows[0].idx
+  }
+
+  public async getByIdx(client: PoolClient, idx: number): Promise<Group> {
+    const query = 'SELECT * FROM groups WHERE idx = $1'
+    const result = await client.query(query, [idx])
+    if (result.rows.length === 0) {
+      throw new NoSuchEntryError()
+    }
+    return this.rowToGroup(result.rows[0])
   }
 
   public async delete(client: PoolClient, groupIdx: number): Promise<number> {
@@ -37,5 +52,19 @@ export default class Groups {
       throw new NoSuchEntryError()
     }
     return result.rows[0].idx
+  }
+
+  private rowToGroup(row: any): Group {
+    return {
+      idx: row.idx,
+      name: {
+        ko: row.name_ko,
+        en: row.name_en,
+      },
+      description: {
+        ko: row.description_ko,
+        en: row.description_en,
+      },
+    }
   }
 }
