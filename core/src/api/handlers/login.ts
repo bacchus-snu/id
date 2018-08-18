f891cc87d19a82f221e27d443303fc2f5bd316d3
skystar-p <usezmap@gmail.com>
2018-08-18 17:53:12 +0900

    login api

---
+++ core/src/api/handlers/login.ts
@@ -0,0 +1,48 @@
+import Model from '../../model/model'
+import { IMiddleware } from 'koa-router'
+import Config from '../../config'
+import { ControllableError } from '../../model/errors'
+
+export function login(model: Model): IMiddleware {
+  return async (ctx, next) => {
+    const body: any = ctx.request.body
+
+    if (!body || typeof body !== 'object') {
+      ctx.status = 400
+      return
+    }
+
+    const { username, password } = body
+    let userId: number
+
+    try {
+      await model.pgDo(async c => {
+        try {
+          userId = await model.users.authenticate(c, username, password)
+
+          if (ctx.session) {
+            // store information in session store
+            ctx.session.userId = userId
+            ctx.session.username = username
+          } else {
+            ctx.status = 500
+            throw new ControllableError('session error')
+          }
+
+        } catch (e) {
+          if (e instanceof ControllableError) {
+            ctx.status = 401
+          } else {
+            ctx.status = 500
+          }
+
+          throw e
+        }
+      })
+    } catch (e) {
+      return
+    }
+
+    await next()
+  }
+}
--- core/src/api/router.ts
+++ core/src/api/router.ts
@@ -1,10 +1,18 @@
 import * as Router from 'koa-router'
 import Model from '../model/model'
 import Config from '../config'
+import { login } from './handlers/login'
 import { getUserList, createUser, deleteUser } from './handlers/users'
 
 export function createRouter(model: Model, config: Config): Router {
   const router = new Router()
+
+  /**
+   * Login API route.
+   * @param username username.
+   * @param password password.
+   */
+  router.post('/api/login', login(model))
 
   router.get('/api/user', getUserList(model))
   router.post('/api/user', createUser(model, config))
