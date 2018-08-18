85969a3d420119ae979cf0f2dfaa71e30b6e8b85
Jangho Seo <jangho@jangho.io>
2018-08-18 17:21:32 +0900

    connect/close

--- migrator/src/index.ts
+++ migrator/src/index.ts
@@ -6,6 +6,7 @@ const config = JSON.parse(fs.readFileSync('config.json', {encoding: 'utf-8'}))
 
 const test = async () => {
   const pool = new mssql.ConnectionPool(config.mssql)
+  await pool.connect()
   const ps = new mssql.PreparedStatement(pool)
   await ps.prepare('SELECT TOP 10 * FROM [users]')
   try {
@@ -13,6 +14,7 @@ const test = async () => {
     console.log(result)
   } finally {
     await ps.unprepare()
+    await pool.close()
   }
 }
 test().catch(e => console.log(e))
