8120188bae7ad3912390dc7fe4d61b16fc81117e
Jangho Seo <jangho@jangho.io>
2018-08-23 23:59:05 +0900

    add WingsUser

--- migrator/src/index.ts
+++ migrator/src/index.ts
@@ -2,19 +2,37 @@ import * as mssql from 'mssql'
 import * as pg from 'pg'
 import * as fs from 'fs'
 
+interface WingsUser {
+  uid: number
+  account: string | null // username
+  name: string // realname
+  password: Buffer | null
+  bs_number: string | null
+  ms_number: string | null
+  phd_number: string | null
+  is_disabled: boolean | null
+}
+
 const config = JSON.parse(fs.readFileSync('config.json', {encoding: 'utf-8'}))
 
-const test = async () => {
-  const pool = new mssql.ConnectionPool(config.mssql)
-  await pool.connect()
-  const ps = new mssql.PreparedStatement(pool)
-  await ps.prepare('SELECT TOP 10 * FROM [user]')
+const migrateUser = async (user: WingsUser, pgClient: pg.PoolClient) => {
+  console.log(user.account)
+}
+
+const migrateAll = async () => {
+  const mssqlPool = new mssql.ConnectionPool(config.mssql)
+  await mssqlPool.connect()
+  const mssqlPS = new mssql.PreparedStatement(mssqlPool)
+  await mssqlPS.prepare('SELECT * FROM [user]')
+  const pgClient = await (new pg.Pool(config.postgresql)).connect()
   try {
-    const result = await ps.execute({})
-    console.log(result)
+    for (const user of (await mssqlPS.execute({})).recordset) {
+      await migrateUser(user, pgClient)
+    }
   } finally {
-    await ps.unprepare()
-    await pool.close()
+    await mssqlPS.unprepare()
+    await mssqlPool.close()
   }
 }
-test().catch(e => console.log(e))
+
+migrateAll().then(_ => console.log('Done')).catch(e => console.log(e))
