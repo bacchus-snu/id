6e91aef8a4aedc6b11eb58be491dc1d408c8c2b4
skystar-p <usezmap@gmail.com>
2018-08-23 15:19:18 +0900

    make whole nav item clickable

--- front/components/TheHeading.vue
+++ front/components/TheHeading.vue
@@ -8,13 +8,13 @@
           </div>
         </a>
       </el-menu-item>
-      <el-menu-item index="2" class="topnav">
+      <el-menu-item @click="routeTo('/')" index="2" class="topnav">
         <nuxt-link to="/">{{ accountTrans[lang] }}</nuxt-link>
       </el-menu-item>
-      <el-menu-item index="3" class="topnav">
+      <el-menu-item @click="routeTo('/lab-support')" index="3" class="topnav">
         <nuxt-link to="/lab-support">{{ labSupportTrans[lang] }}</nuxt-link>
       </el-menu-item>
-      <el-menu-item index="4" class="topnav">
+      <el-menu-item @click="routeTo('/appcenter')" index="4" class="topnav">
         <nuxt-link to="/appcenter">{{ appCenterTrans[lang] }}</nuxt-link>
       </el-menu-item>
       <el-menu-item index="5" class="topnav alink">
@@ -31,7 +31,7 @@
           </div>
         </a>
       </el-menu-item>
-      <el-menu-item index="7" class="topnav alink">
+      <el-menu-item @click="routeTo('/help')" index="7" class="topnav alink">
         <nuxt-link to="/help">{{ helpTrans[lang] }}<i class="el-icon-question"></i></nuxt-link>
       </el-menu-item>
       <el-button class="topbutton" type="warning" @click="changeLang">{{ langTrans[lang] }}</el-button>
@@ -91,6 +91,10 @@ export default class TheHeading extends Vue {
 
   get lang(): Language {
     return this.$store.state.language
+  }
+
+  public routeTo(route: string) {
+    this.$router.push(route)
   }
 }
 </script>
