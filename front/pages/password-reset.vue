76513e65fce3043affa1d099ea34a64c95ae4a71
hjbc0921 <hjbc0921@gmail.com>
2018-08-26 01:38:27 +0900

    password reset page

---
+++ front/pages/password-reset.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <find-password></find-password>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import FindPassword from '~/components/FindPassword'
+import axios from 'axios'
+
+@Component({
+  components: {
+    'find-password': FindPassword,
+  },
+})
+
+export default class PasswordResetPage extends Vue {
+
+}
+</script>
