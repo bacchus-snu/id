a6db85b91a190bfba65df946216354c4ade593e0
Jangho Seo <jangho@jangho.io>
2018-08-30 20:44:28 +0900

    migrator: remove dup

--- migrator/src/index.ts
+++ migrator/src/index.ts
@@ -17,7 +17,7 @@ const config = JSON.parse(fs.readFileSync('config.json', {encoding: 'utf-8'}))
 
 const failed: Array<string> = []
 
-const migrateUser = async (user: WingsUser, pgClient: pg.PoolClient, selectEmail: mssql.PreparedStatement) => {
+const migrateUser = async (user: WingsUser, duplicates: Array<string>, pgClient: pg.PoolClient, selectEmail: mssql.PreparedStatement) => {
   if (user.account === null) {
     return
   }
@@ -25,7 +25,7 @@ const migrateUser = async (user: WingsUser, pgClient: pg.PoolClient, selectEmail
   const addresses: Array<string> = []
   for (const addrRecord of (await selectEmail.execute({userUid: user.uid})).recordset) {
     const address = addrRecord.email
-    if (!addresses.includes(address)) {
+    if (!duplicates.includes(address)) {
       addresses.push(address)
     }
   }
@@ -45,17 +45,25 @@ const migrateUser = async (user: WingsUser, pgClient: pg.PoolClient, selectEmail
     }
   }
 
-  if (addressIdxs.length === 0) {
-    console.error(`${user.account} has no email addresses`)
-    return
-  }
-
-  const userInsertResult = await pgClient.query('INSERT INTO users (username, name, shell, preferred_language, primary_email_address_idx) ' +
-    'VALUES ($1, $2, \'/bin/bash\', \'ko\', $3) RETURNING idx', [user.account, user.name, addressIdxs[0]])
+  const userInsertResult = await pgClient.query('INSERT INTO users (username, name, shell, preferred_language) ' +
+    'VALUES ($1, $2, \'/bin/bash\', \'ko\') RETURNING idx', [user.account, user.name])
   const userIdx = userInsertResult.rows[0].idx
 
   for (const emailAddressIdx of addressIdxs) {
     await pgClient.query('UPDATE email_addresses SET owner_idx=$1 where idx=$2', [userIdx, emailAddressIdx])
+  }
+}
+
+const findDup = (allEmails: Array<string>) => {
+  const found: Array<string> = []
+  const dup: Array<string> = []
+  for (const email of allEmails) {
+    if (found.includes(email)) {
+      console.error(`dup: ${email}`)
+      dup.push(email)
+    } else {
+      found.push(email)
+    }
   }
 }
 
@@ -67,14 +75,19 @@ const migrateAll = async () => {
   const mssqlSelectEmail = new mssql.PreparedStatement(mssqlPool)
   mssqlSelectEmail.input('userUid', mssql.BigInt)
   await mssqlSelectEmail.prepare('SELECT * FROM user_email WHERE user_uid=@userUid')
+  const mssqlSelectAllEmail = new mssql.PreparedStatement(mssqlPool)
+  await mssqlSelectAllEmail.prepare('SELECT * FROM user_email')
   const pgClient = await (new pg.Pool(config.postgresql)).connect()
   try {
+    const allEmails = (await mssqlSelectAllEmail.execute({})).recordset
+    const duplicates = findDup(allEmails)
     for (const user of (await mssqlSelectUser.execute({})).recordset) {
-      await migrateUser(user, pgClient, mssqlSelectEmail)
+      await migrateUser(user, duplicates, pgClient, mssqlSelectEmail)
     }
   } finally {
     await mssqlSelectUser.unprepare()
     await mssqlSelectEmail.unprepare()
+    await mssqlSelectAllEmail.unprepare()
     await mssqlPool.close()
     await pgClient.release()
   }
