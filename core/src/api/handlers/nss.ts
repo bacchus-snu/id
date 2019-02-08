2fa39882dc660a6d916fad1035fb7d9c166ef396
Yongun Seong <nevivurn@riseup.net>
2019-02-08 19:39:09 +0900

    Move passwd/group generation to models
    This gives us almost-proper cache invalidation.
    For truly correct invalidation we would need LISTEN/NOTIFY,
    but probably not necessary.

--- core/src/api/handlers/nss.ts
+++ core/src/api/handlers/nss.ts
@@ -1,52 +1,9 @@
 import Model from '../../model/model'
 import { User } from '../../model/users'
-import Config from '../../config'
 import { IMiddleware } from 'koa-router'
 import { NoSuchEntryError } from '../../model/errors'
-import util = require('util')
 
-let encodedPasswd = ''
-let encodedGroup = ''
-let lastModified = new Date()
-
-async function update(model: Model, config: Config) {
-  const users = await model.pgDo(async tr => {
-     return await model.users.getAll(tr)
-  })
-
-  // TODO: push this to the db layer?
-  users.sort((a, b) => {
-    return a.uid - b.uid
-  })
-
-  let passwd = ''
-  const usernames: Array<string> = []
-  users.forEach(user => {
-    passwd += util.format('%s:x:%d:%d:%s:%s:%s\n',
-      user.username, user.uid, config.posix.userGroupGid, user.name,
-      `${model.config.posix.homeDirectoryPrefix}/${user.username}`,
-      user.shell)
-
-    if (user.username) {
-      usernames.push(user.username)
-    }
-  })
-
-  if (passwd !== encodedPasswd) {
-    encodedPasswd = passwd
-    encodedGroup = util.format('%s:x:%d:%s',
-      config.posix.userGroupName, config.posix.userGroupGid,
-      usernames.join(','))
-    lastModified = new Date()
-  }
-}
-
-export function getPasswd(model: Model, config: Config): IMiddleware {
-  // TODO: invalidate on user change, move to models
-  setInterval(() => {
-    update(model, config)
-  }, 15 * 60)
-
+export function getPasswd(model: Model): IMiddleware {
   return async (ctx, next) => {
     try {
       await model.pgDo(async tr => {
@@ -61,20 +18,30 @@ export function getPasswd(model: Model, config: Config): IMiddleware {
         return
     }
 
+    let passwd = ''
+    try {
+      passwd = await model.pgDo(async tr => {
+        return model.users.getPasswdEntries(tr)
+      })
+      ctx.lastModified = model.users.getPosixLastModified()
+    } catch (e) {
+      ctx.status = 500
+      return
+    }
+
     ctx.status = 200 // Required for freshness check
-    ctx.lastModified = lastModified
 
     if (ctx.fresh) {
       ctx.status = 304
     } else {
-      ctx.body = encodedPasswd
+      ctx.body = passwd
     }
 
     await next()
   }
 }
 
-export function getGroup(model: Model, config: Config): IMiddleware {
+export function getGroup(model: Model): IMiddleware {
   return async (ctx, next) => {
     try {
       await model.pgDo(async tr => {
@@ -89,13 +56,23 @@ export function getGroup(model: Model, config: Config): IMiddleware {
         return
     }
 
+    let group = ''
+    try {
+      group = await model.pgDo(async tr => {
+        return model.users.getGroupEntries(tr)
+      })
+      ctx.lastModified = model.users.getPosixLastModified()
+    } catch (e) {
+      ctx.status = 500
+      return
+    }
+
     ctx.status = 200 // Required for freshness check
-    ctx.lastModified = lastModified
 
     if (ctx.fresh) {
       ctx.status = 304
     } else {
-      ctx.body = encodedGroup
+      ctx.body = group
     }
 
     await next()
