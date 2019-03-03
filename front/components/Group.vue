e857696a62b2aa7b7e9af290ba7c700f1d248e55
Changmin Choi <cmchoi9901@gmail.com>
2019-03-03 23:39:09 +0900

    matched variables of group api

--- front/components/Group.vue
+++ front/components/Group.vue
@@ -4,34 +4,34 @@
     <el-row>
       <el-col :md="{span: 8, offset: 8}" :sm="{span: 24, offset: 0}" :xs="{span: 24, offset: 0}">
         <el-table :data="groupList" height="250" style="width: 100%">
-          <el-table-column prop="groupName" label="Group Name" width="180"></el-table-column>
+          <el-table-column prop="name" label="Group Name" width="180"></el-table-column>
           <el-table-column label="Status" width="180">
             <template slot-scope="slotProps">
               <el-button
-                v-if="slotProps.row.isPending"
+                v-if="slotProps.row.is_pending"
                 disabled
                 size="mini"
-                @click="applyGroup(groupId)"
+                @click="applyGroup(slotProps.row.idx)"
               >Pending</el-button>
               <el-button
-                v-else-if="slotProps.row.isMember"
+                v-else-if="slotProps.row.is_member"
                 size="mini"
                 type="danger"
-                @click="leaveGroup(groupId)"
+                @click="leaveGroup(slotProps.row.idx)"
               >Leave</el-button>
-              <el-button v-else size="mini" @click="applyGroup(groupId)">Apply</el-button>
+              <el-button v-else size="mini" @click="applyGroup(slotProps.row.idx)">Apply</el-button>
             </template>
           </el-table-column>
           <el-table-column label="Ownership" width="180">
             <template slot-scope="slotProps">
               <el-button
-                v-if="slotProps.row.isOwner"
+                v-if="slotProps.row.is_owner"
                 size="mini"
-                @click="GroupOwnership(groupId)"
+                @click="GroupOwnership(slotProps.row.idx)"
               >GroupAdministration</el-button>
             </template>
           </el-table-column>
-          <el-table-column prop="explanation" label="Explanation"></el-table-column>
+          <el-table-column prop="description" label="Description"></el-table-column>
         </el-table>
       </el-col>
     </el-row>
@@ -97,8 +97,8 @@ export default class Group extends Vue {
     this.groupList = groupResponse.data
   }
 
-  private async applyGroup(groupId: number) {
-    const response = await axios.post("/api/apply-group", groupId);
+  private async applyGroup(idx: number) {
+    const response = await axios.post("/api/group/" + idx + "/apply");
     if (response.status === 200) {
       this.$router.push("/group");
     } else {
@@ -107,8 +107,8 @@ export default class Group extends Vue {
     }
   }
 
-  private async leaveGroup(groupId: number) {
-    const response = await axios.post("/api/leave-group", groupId);
+  private async leaveGroup(idx: number) {
+    const response = await axios.post("/api/group/" + idx + "/leave");
     if (response.status === 200) {
       this.$router.push("/group");
     } else {
@@ -117,8 +117,8 @@ export default class Group extends Vue {
     }
   }
 
-  private async GroupOwnerShip(groupId: number) {
-    this.$router.push("/group/" + groupId);
+  private async GroupOwnerShip(idx: number) {
+    this.$router.push("/group/" + idx);
   }
 
   data() {
