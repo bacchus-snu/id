938c443ee998b3653481a7bdb04dbbf379ca8e25
WSL_UBUNTU_KJY <jykim157@naver.com>
2021-08-08 16:22:40 +0900

    eslint-core migration from tslint

---
+++ core/.eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    'no-extra-semi': 'error',
+    'no-sequences': 'error',
+    quotes: ['error', 'single'],
+    'sort-keys': 'off',
+    'sort-imports': 'off',
+    '@typescript-eslint/array-type': ['error', { default: 'generic' }],
+    'arrow-parens': ['error', 'as-needed'],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'default',
+        format: ['camelCase'],
+        leadingUnderscore: 'allow',
+        trailingUnderscore: 'allow',
+      },
+
+      {
+        selector: 'variable',
+        format: ['camelCase', 'UPPER_CASE'],
+        leadingUnderscore: 'allow',
+        trailingUnderscore: 'allow',
+      },
+
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+      },
+      {
+        selector: 'typeProperty',
+        format: ['camelCase', 'snake_case'],
+      },
+    ],
+  },
+};
