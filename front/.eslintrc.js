2c75742c25177773c02f342e0d097ae26b7968fa
Wonwoo Choi <vbchunguk@bacchus.snucse.org>
2021-08-08 16:11:28 +0900

    front: Use ESLint

---
+++ front/.eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/essential',
+  ],
+  rules: {
+    'semi': ['error', 'never'],
+    'no-sequences': 'error',
+    'quotes': ['error', 'single'],
+    'arrow-parens': ['error', 'as-needed'],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'default',
+        format: ['camelCase'],
+        leadingUnderscore: 'allow',
+        trailingUnderscore: 'allow',
+      },
+      {
+        selector: 'variable',
+        format: ['camelCase', 'UPPER_CASE'],
+        leadingUnderscore: 'allow',
+        trailingUnderscore: 'allow',
+      },
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+      },
+      {
+        selector: 'typeProperty',
+        format: ['camelCase', 'snake_case'],
+      },
+    ],
+    '@typescript-eslint/array-type': ['error', { default: 'generic' }],
+  },
+}
