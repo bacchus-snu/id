0a228cf03d00e9c8644b5ecb87e6050f3b3e96c0
Wonwoo Choi <vbchunguk@bacchus.snucse.org>
2023-06-19 19:49:50 +0900

    가입 토큰을 request body로도 받을 수 있도록 함

--- core/src/api/handlers/users.ts
+++ core/src/api/handlers/users.ts
@@ -22,12 +22,12 @@ export function createUser(model: Model, config: Config): IMiddleware {
       return
     }
 
-    if (!ctx.session.verificationToken) {
+    if (!body.token && !ctx.session.verificationToken) {
       ctx.status = 401
       return
     }
 
-    const token = ctx.session.verificationToken
+    const token = body.token || ctx.session.verificationToken
     let emailAddress: EmailAddress
 
     // check verification token
