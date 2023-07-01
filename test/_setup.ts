a96a45a66f6b6b47ccad7353accf85199296dc14
Wonwoo Choi <vbchunguk@bacchus.snucse.org>
2023-07-01 21:10:15 +0900

    Fix test app

--- test/_setup.ts
+++ test/_setup.ts
@@ -1,6 +1,6 @@
+import { Server } from 'node:http'
 import test from 'ava'
 import * as fs from 'fs'
-import Koa from 'koa'
 import createAPIServer from '../src/api/server'
 import Logger, * as bunyan from 'bunyan'
 import Model from '../src/model/model'
@@ -8,7 +8,7 @@ import Model from '../src/model/model'
 export let config: any
 export let log: Logger
 export let model: Model
-export let app: Koa
+export let app: Server
 test.before(async () => {
   config = JSON.parse(await fs.promises.readFile('config.test.json', {encoding: 'utf-8'}))
   log = bunyan.createLogger({
@@ -16,6 +16,6 @@ test.before(async () => {
     level: config.logLevel,
   })
   model = new Model(config, log)
-  app = await createAPIServer(config, log, model)
-  app.listen()
+  const koa = await createAPIServer(config, log, model)
+  app = koa.listen()
 })
