35fdf317df9b1480c3ff1deaa62e0ed70f2d8d62
Wonwoo Choi <vbchunguk@bacchus.snucse.org>
2023-07-30 15:10:25 +0900

    OAuth 클라이언트 각종 버그 수정

--- src/model/oauth.ts
+++ src/model/oauth.ts
@@ -6,7 +6,7 @@ import Transaction from './transaction'
 
 export default class OAuth {
   public async getClientById(tr: Transaction, id: string): Promise<AllClientMetadata> {
-    const client = await tr.query('SELECT client_id, client_secret, client_name FROM oauth_client WHERE client_id = $1', [id])
+    const client = await tr.query('SELECT client_id, client_secret, client_name FROM oauth_clients WHERE client_id = $1', [id])
     const redirectUri = await tr.query('SELECT redirect_uri FROM oauth_client_redirect_uris WHERE client_id = $1', [id])
     if (client.rows.length !== 1) {
       throw new NoSuchEntryError()
@@ -17,11 +17,12 @@ export default class OAuth {
       client_secret: client.rows[0].client_secret,
       client_name: client.rows[0].client_name,
       redirect_uris: redirectUri.rows.map(row => row.redirect_uri),
+      id_token_signed_response_alg: 'ES256',
     }
   }
 
   public async isFirstParty(tr: Transaction, id: string): Promise<boolean> {
-    const result = await tr.query('SELECT first_party FROM oauth_client WHERE client_id = $1', [id])
+    const result = await tr.query('SELECT first_party FROM oauth_clients WHERE client_id = $1', [id])
     return Boolean(result.rows[0]?.first_party)
   }
 }
